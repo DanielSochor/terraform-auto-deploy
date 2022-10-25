14a172ae27923c6a240fb2e561ddf4a56ad71487
DanielSochor <danielsochor@gmail.com>
2022-10-25 14:15:59 -0500

    adjusted spacing and used terraform fmt

--- main.tf
+++ main.tf
@@ -1,45 +1,43 @@
 terraform {
-    required_providers {
+  required_providers {
     # contains terraform settings, including the required providers Terraform will use to provision infrastructure
-        aws = {
-            source  = "hashicorp/aws"
-        # source is the global source address
-        # source defines an optional hostname, namespace, provider type
-        # providers are installed from the Terraform Registry by default
-        # hashicorp/aws is shorthand for registty.terraform.io/hashicorp/aws
-            version = "~> 4.16"
-        # version is optional but recommended
-        }
+    aws = {
+      source = "hashicorp/aws"
+      # source is the global source address
+      # source defines an optional hostname, namespace, provider type
+      # providers are installed from the Terraform Registry by default
+      # hashicorp/aws is shorthand for registty.terraform.io/hashicorp/aws
+      version = "~> 4.16"
+      # version is optional but recommended
     }
-    
-    cloud {
-        organization = "github-auto-deploy"
+  }
+  cloud {
+    organization = "github-auto-deploy"
 
-        workspaces {
-            name = "gh-actions-demo"
-        }
-        
+    workspaces {
+      name = "gh-actions-demo"
     }
-    
+
+  }
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-# provider block configures the specified provider
-# a provider is a plugin Terraform uses to create and manage your resources
-  region  = "us-east-2"
+  # provider block configures the specified provider
+  # a provider is a plugin Terraform uses to create and manage your resources
+  region = "us-east-2"
 }
 
 resource "aws_instance" "tf_created_instance" {
-# a resource might be physical or virtual component or a logical resource
-# a resource has two strings before the block: resource type and resource name
-#   resource type prefix maps to the name of the provider
-#   the resource type and name together form a unique ID for the resource
+  # a resource might be physical or virtual component or a logical resource
+  # a resource has two strings before the block: resource type and resource name
+  #   resource type prefix maps to the name of the provider
+  #   the resource type and name together form a unique ID for the resource
   ami           = "ami-089a545a9ed9893b6"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Intance created by Terraform"
+    Name = "Instance created by Terraform"
   }
 }
 
