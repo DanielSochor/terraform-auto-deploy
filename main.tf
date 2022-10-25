671f2095ce93fe3683a55e34f61ca6dd1b100a77
DanielSochor <danielsochor@gmail.com>
2022-10-25 10:19:24 -0500

    add comments to resource block

--- main.tf
+++ main.tf
@@ -22,6 +22,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
+# a resource might be physical or virtual component or a logical resource
+# a resource has two strings before the block: resource type and resource name
+#   resource type prefix maps to the name of the provider
+#   the resource type and name together form a unique ID for the resource
   ami           = "ami-089a545a9ed9893b6"
   instance_type = "t2.micro"
 
