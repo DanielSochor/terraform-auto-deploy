3f7211adc596387609205e07230030cf9de8f028
DanielSochor <danielsochor@gmail.com>
2022-10-26 12:26:43 -0500

    does commenting out the instance resource in the main.tf remove the instance?

--- main.tf
+++ main.tf
@@ -28,15 +28,15 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "tf_created_instance" {
-  # a resource might be physical or virtual component or a logical resource
-  # a resource has two strings before the block: resource type and resource name
-  #   resource type prefix maps to the name of the provider
-  #   the resource type and name together form a unique ID for the resource
-  ami           = "ami-089a545a9ed9893b6"
-  instance_type = "t2.micro"
+# resource "aws_instance" "tf_created_instance" {
+#   # a resource might be physical or virtual component or a logical resource
+#   # a resource has two strings before the block: resource type and resource name
+#   #   resource type prefix maps to the name of the provider
+#   #   the resource type and name together form a unique ID for the resource
+#   ami           = "ami-089a545a9ed9893b6"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "Instance created by Terraform"
-  }
-}
+#   tags = {
+#     Name = "Instance created by Terraform"
+#   }
+# }
