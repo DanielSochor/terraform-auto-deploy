96188130528773dbb8e2feecaff84194eb80b0ca
DanielSochor <danielsochor@gmail.com>
2022-10-27 15:28:53 -0500

    add tag to name the instance

--- main.tf
+++ main.tf
@@ -48,4 +48,7 @@ module "ec2_created_by_local_module" {
 module "ec2_created_by_terraform_provided_module" {
   source = "terraform-aws-modules/ec2-instance/aws"
   ami    = "ami-089a545a9ed9893b6"
+  tags = {
+    Name = "Created by Terraform Provided Module"
+  }
 }
